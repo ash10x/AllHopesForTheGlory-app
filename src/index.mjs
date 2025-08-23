ae91bc3bddba75af9d10d3130d656be0b0acce14
Rodique Johnson <rodiquejohnson@outlook.com>
2025-08-23 17:21:55 -0500

    .

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,17 @@
 // Imports...
-import express from 'express'
+import express from "express";
 
 // Root Router
-import rootRouter from './routes/index.mjs'
+import rootRouter from "./routes/index.mjs";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(rootRouter)
-
+app.use(express.json());
+app.use(express.static("dist"));
+app.use(rootRouter);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Running | PORT: ${PORT}`)
-})
-
+  console.log(`Server Running | PORT: ${PORT}`);
+});
