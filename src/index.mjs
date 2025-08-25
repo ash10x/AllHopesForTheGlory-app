7d88d8770a990eecd14ac9383f420d724d850d0d
Rodique Johnson <rodiquejohnson@outlook.com>
2025-08-25 00:07:55 -0500

    .

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,10 @@
 // Imports...
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Root Router
 import rootRouter from "./routes/index.mjs";
@@ -7,8 +12,12 @@ import rootRouter from "./routes/index.mjs";
 const app = express();
 
 app.use(express.json());
-app.use(express.static("dist"));
 app.use(rootRouter);
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
+app.get("/*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
