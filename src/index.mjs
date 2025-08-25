4e3d59b1ba36b46aa653d5229c147c47787ece19
Rodique Johnson <rodiquejohnson@outlook.com>
2025-08-24 22:43:14 -0500

    .

--- src/index.mjs
+++ src/index.mjs
@@ -7,13 +7,13 @@ import rootRouter from "./routes/index.mjs";
 const app = express();
 
 app.use(express.json());
-app.use(express.static("dist"));
+app.use(express.static(".vercel/output/static"));
 app.use(rootRouter);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.sendFile("dist", "index.html");
+  res.sendFile(".vercel/output/static", "index.html");
 });
 
 app.listen(PORT, () => {
