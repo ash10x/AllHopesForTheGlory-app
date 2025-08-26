02736f854c3264b3b5321e38c8833ac79740a368
Rodique Johnson <rodiquejohnson@outlook.com>
2025-08-25 22:38:24 -0500

    .

--- src/routes/contact.mjs
+++ src/routes/contact.mjs
@@ -13,7 +13,7 @@ router.use(
   })
 );
 
-router.post("/api/contact", async (req, res) => {
+router.post("/api/contact", (req, res) => {
   const data = [
     req.body.fullname,
     req.body.phonenumber,
@@ -22,74 +22,72 @@ router.post("/api/contact", async (req, res) => {
     req.body.message,
   ];
 
-  try {
-    const result = await database.query(
-      "SELECT * FROM contacts RETURNING *",
-      data
-    );
-    res.json(result.rows);
-  } catch (err) {
-    console.error("Error adding user", err);
-    res.status(500).send("Internal Server Error");
-  }
+  // database.query("SELECT * FROM contacts", (error, result) => {
+  //   if (error) {
+  //     res.send(false);
+  //     console.log(error);
+  //   } else {
+  //     res.send(true);
+  //   }
+  // });
 
-  //   database.query(
-  //     "INSERT INTO contacts (fullname, phonenumber, email, companyname, message) VALUES($1, $2, $3, $4, $5)",
-  //     data,
-  //     (error, result) => {
-  //       if (result.rowCount > 0) {
-  //         const mailTransporter = nodemailer.createTransport({
-  //           host: process.env.SMTP_SERVICE,
-  //           secure: true,
-  //           port: process.env.PORT,
-  //           auth: {
-  //             user: process.env.SMTP_USER,
-  //             pass: process.env.SMTP_PASS,
-  //           },
-  //         });
+  database.query(
+    "INSERT INTO contacts (fullname, phonenumber, email, companyname, message) VALUES($1, $2, $3, $4, $5)",
+    data,
+    (error, result) => {
+      if (result.rowCount > 0) {
+        const mailTransporter = nodemailer.createTransport({
+          host: process.env.SMTP_SERVICE,
+          secure: true,
+          port: process.env.PORT,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+          },
+        });
 
-  //         const mailOptions = {
-  //           from: "All Hopes For The Glory ©" + " " + process.env.SMTP_USER,
-  //           to: [process.env.LEAD_EMAIL, process.env.SMTP_USER],
-  //           subject: "Service Request",
-  //           html: `<h1>New Service Request</h1>
-  //                    <p>A new lead was generated from the All Hopes For The Glory Website.</p>
-  //                    <p><strong>Name</strong> 👤: ${req.body.fullname}</p>
-  //                    <p><strong>Phone</strong> 📞: '+' + ${req.body.phonenumber}</p>
-  //                    <p><strong>Email</strong> 📧: ${req.body.email}</p>
-  //                    <p><strong>Company Name</strong> 🏛️: ${req.body.companyname}</p>
-  //                    <p><strong>Message</strong><br> ${req.body.message}</p>`,
-  //         };
-  //         const ConfirmMailOptions = {
-  //           from: "Request Received" + " " + process.env.SMTP_USER,
-  //           to: req.body.email,
-  //           subject: "Request Confirmation",
-  //           html: `<h2>Your request was received!</h2>
-  //                    <p>Hi ${req.body.fullname},</p>
-  //                    <p>Thank you for your submission. Our team is currently reviewing your request and you can expect a response within 24 hours. We appreciate your patience as we work to provide you with the best service possible.</p><br>
-  //                    <p><i>Please do not reply to this email.</i></p>`,
-  //         };
+        const mailOptions = {
+          from: "All Hopes For The Glory ©" + " " + process.env.SMTP_USER,
+          to: [process.env.LEAD_EMAIL, process.env.SMTP_USER],
+          subject: "Service Request",
+          html: `<h1>New Service Request</h1>
+                     <p>A new lead was generated from the All Hopes For The Glory Website.</p>
+                     <p><strong>Name</strong> 👤: ${req.body.fullname}</p>
+                     <p><strong>Phone</strong> 📞: '+' + ${req.body.phonenumber}</p>
+                     <p><strong>Email</strong> 📧: ${req.body.email}</p>
+                     <p><strong>Company Name</strong> 🏛️: ${req.body.companyname}</p>
+                     <p><strong>Message</strong><br> ${req.body.message}</p>`,
+        };
+        const ConfirmMailOptions = {
+          from: "Request Received" + " " + process.env.SMTP_USER,
+          to: req.body.email,
+          subject: "Request Confirmation",
+          html: `<h2>Your request was received!</h2>
+                     <p>Hi ${req.body.fullname},</p>
+                     <p>Thank you for your submission. Our team is currently reviewing your request and you can expect a response within 24 hours. We appreciate your patience as we work to provide you with the best service possible.</p><br>
+                     <p><i>Please do not reply to this email.</i></p>`,
+        };
 
-  //         mailTransporter.sendMail(mailOptions, function (error, info) {
-  //           if (error) {
-  //             console.log(error);
-  //           } else {
-  //             res.send(true);
-  //           }
-  //         });
+        mailTransporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send(true);
+          }
+        });
 
-  //         mailTransporter.sendMail(ConfirmMailOptions, function (error, info) {
-  //           if (error) {
-  //             console.log(error);
-  //           } else {
-  //             res.send(true);
-  //           }
-  //         });
-  //       } else {
-  //         res.send(false);
-  //       }
-  //     }
-  //   );
+        mailTransporter.sendMail(ConfirmMailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send(true);
+          }
+        });
+      } else {
+        res.send(false);
+      }
+    }
+  );
 });
 
 export default router;
--- src/routes/databaseconnect.mjs
+++ src/routes/databaseconnect.mjs
@@ -1,8 +1,15 @@
-import { Pool } from 'pg'
-import 'dotenv/config'
+import { Pool } from "pg";
+import "dotenv/config";
 
-   const database = new Pool({
-     connectionString: process.env.DATABASE_URL
-   });
+const database = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-export default database
+export default database;
